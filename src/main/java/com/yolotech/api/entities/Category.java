b497fe456c91bb3284baad8ae97f34ea6f98188c
Fabio Frasson <fabio.frass@gmail.com>
2021-05-08 19:25:01 -0300

    H2 Database, test profile & JPA

--- src/main/java/com/yolotech/api/entities/Category.java
+++ src/main/java/com/yolotech/api/entities/Category.java
@@ -2,8 +2,13 @@ package com.yolotech.api.entities;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
@@ -11,6 +16,8 @@ import java.io.Serializable;
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter @Setter private Long id;
     @Getter @Setter private String name;
     @Getter @Setter private boolean isActive;
